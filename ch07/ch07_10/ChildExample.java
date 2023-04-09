179dc9444be03836f4369daf888eb4635236c647
khs220507 <khs220507@gmail.com>
2023-04-10 08:08:41 +0900

    practice_java

---
+++ ch07/ch07_10/ChildExample.java
@@ -0,0 +1,17 @@
+package ch07_10;
+
+public class ChildExample {
+	
+	public static void main(String[] args) {
+		
+		Child child = new Child();
+		
+		Parent parent = child;
+		
+		
+		parent.method1();
+		parent.method2();
+		 
+	}
+
+}
